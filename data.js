0e1f5a1e19ea1631b0480e348eccc83a8bb2538f
sidharth.rama@gmail.com <sidharth.sundaram@upgrad.com>
2022-04-12 12:30:48 +0530

    added comments for js vs json

---
+++ data.js
@@ -0,0 +1,20 @@
+//no quotes don't work
+{
+    id: 101,
+    name: "Sid",
+    developer: true
+ }
+
+//single quotes work in JS
+ {
+    'id': 101,
+    'name': "Sid",
+    'developer': true
+ }
+
+ //double quotes work in JS
+ {
+    "id": 101,
+    "name": "Srishti",
+    "developer": true
+ }
